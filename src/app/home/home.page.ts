475adf0480d28288da31fb11ba75d176e01bb951
OswalGGama-PCA <oswaldo.gamarras@pca.edu.co>
2026-01-15 21:18:43 -0500

    feat: Implement modern slides with theme switching and progress bar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,28 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonContent, IonFab, IonFabButton, IonIcon, IonProgressBar } from '@ionic/angular/standalone';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, IonContent, IonFab, IonFabButton, IonIcon, IonProgressBar],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  currentTheme = 'light';
+  themes = ['light', 'dark', 'ocean', 'sunset', 'forest'];
 
-  genres = [
+  progress = 0;
 
-  {
-    title: "Musica de Perreo",
-    img:"assets/perreo.png",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+  artists: any[] = [];
+  isLoading = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadArtists();
   }
-  ]
 
-  constructor() {}
+  loadArtists() {
+    const artistNames = [
+      'Coldplay', 'Metallica', 'Beyonce', 'Bad Bunny', 'Daft Punk', 'Marc Anthony',
+      'Taylor Swift', 'Ed Sheeran', 'Ariana Grande', 'Drake', 'Rihanna',
+      'The Weeknd', 'Billie Eilish', 'Justin Bieber', 'Lady Gaga', 'Bruno Mars',
+      'Adele', 'Queen', 'The Beatles', 'Michael Jackson', 'Madonna', 'Eminem',
+      'Shakira', 'U2', 'Bob Marley', 'David Bowie'
+    ];
+    
+    artistNames.forEach(name => {
+      this.http.get(`https://www.theaudiodb.com/api/v1/json/2/search.php?s=${name}`)
+        .subscribe({
+          next: (data: any) => {
+            if (data.artists && data.artists.length > 0) {
+              const artist = data.artists[0];
+              this.artists.push({
+                title: artist.strArtist,
+                img: artist.strArtistThumb || artist.strArtistBanner || 'assets/default-artist.png',
+                description: (artist.strBiographyEN || artist.strBiographyES || 'No description available').substring(0, 200) + '...',
+                genre: artist.strGenre,
+                country: artist.strCountry
+              });
+            }
+          },
+          error: (error) => {
+            console.error('Error loading artist:', error);
+          },
+          complete: () => {
+            this.isLoading = false;
+          }
+        });
+    });
+  }
 
+  onSlideChange(event: any) {
+    const [swiper] = event.detail;
+    if (this.artists.length > 0) {
+      this.progress = (swiper.realIndex + 1) / this.artists.length;
+    }
+  }
 
+  cambiarColor() {
+    const currentIndex = this.themes.indexOf(this.currentTheme);
+    const nextIndex = (currentIndex + 1) % this.themes.length;
+    
+    // Remover tema anterior
+    document.body.classList.remove(`${this.currentTheme}-theme`);
+    
+    // Aplicar nuevo tema
+    this.currentTheme = this.themes[nextIndex];
+    document.body.classList.add(`${this.currentTheme}-theme`);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
-register();
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes),
+    provideHttpClient()
   ],
 });
